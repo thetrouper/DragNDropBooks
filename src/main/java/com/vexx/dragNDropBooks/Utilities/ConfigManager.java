e56ff5bcb798facfd246acbede7590f2743b99a9
Vexx <Andrew@Thinkinthecloud.com>
2024-07-29 22:23:31 -0400

    removing unused config item

--- src/main/java/com/vexx/dragNDropBooks/Utilities/ConfigManager.java
+++ src/main/java/com/vexx/dragNDropBooks/Utilities/ConfigManager.java
@@ -8,15 +8,11 @@ import java.util.Map;
 
 public class ConfigManager {
     public boolean cost_settings_enabled ;
-    public int player_level_cost_per_enchant_level;
     public double refund_rate;
     public final  Map<Enchantment, Integer> enchant_costs = new HashMap<Enchantment, Integer>();
-
-
+    
     public ConfigManager(DragNDropBooks main){
         this.cost_settings_enabled = main.getConfig().getBoolean("cost_settings.enabled");
-        this.player_level_cost_per_enchant_level =
-            main.getConfig().getInt("cost_settings.player_level_cost_per_enchant_level");
         this.refund_rate = main.getConfig().getDouble("refund_settings.refund_rate");
 
         enchant_costs.put(Enchantment.PROTECTION,
