6cefc3c80bd65e515f578b59fb12e4ab0fa7d2b5
thetrouper <crvn152006@gmail.com>
2025-04-03 19:52:51 -0500

    Bumped to 1.1, added config toggle for the disenchant feature and commented out the println verbose. Also removed the unused variable and put a null check for the main command registration.

--- src/main/java/com/vexx/dragNDropBooks/DragNDropBooks.java
+++ src/main/java/com/vexx/dragNDropBooks/DragNDropBooks.java
@@ -5,13 +5,13 @@ import com.vexx.dragNDropBooks.Enchanting.Enchanter;
 import com.vexx.dragNDropBooks.Utilities.ConfigManager;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
-import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.command.PluginCommand;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
 
 public final class DragNDropBooks extends JavaPlugin implements Listener {
 
@@ -20,8 +20,8 @@ public final class DragNDropBooks extends JavaPlugin implements Listener {
         Bukkit.getPluginManager().registerEvents(this, this);
         getConfig().options().copyDefaults();
         saveDefaultConfig();
-        FileConfiguration config = getConfig();
-        getCommand("dragndropbooks").setExecutor(new Commands(this));
+        PluginCommand mainCommand = getCommand("dragndropbooks");
+        if (mainCommand != null) mainCommand.setExecutor(new Commands(this));
     }
 
     @EventHandler
@@ -29,15 +29,15 @@ public final class DragNDropBooks extends JavaPlugin implements Listener {
         ItemStack enchantedBook = e.getCursor();
         ItemStack item = e.getCurrentItem();
         Player player = (Player) e.getWhoClicked();
-        if(enchantedBook == null
+        if (enchantedBook == null
           || item == null
           || enchantedBook.getType().isAir()
           || item.getType().isAir()
           || enchantedBook.getType() != Material.ENCHANTED_BOOK) return;
-        System.out.println("enchantedBook: " + enchantedBook);
-        System.out.println("item: " + item);
+        //System.out.println("enchantedBook: " + enchantedBook);
+        //System.out.println("item: " + item);
         Enchanter enchanter = new Enchanter(player, enchantedBook, item, new ConfigManager(this));
-        if(!enchanter.isValidItemStacks()) return;
+        if (!enchanter.isValidItemStacks()) return;
         enchanter.applyEnchantment();
         e.setCancelled(true);
         e.setCurrentItem(enchanter.getItem());
@@ -48,11 +48,16 @@ public final class DragNDropBooks extends JavaPlugin implements Listener {
         ItemStack book = e.getCurrentItem();
         ItemStack enchantedItem = e.getCursor();
         Player player = (Player) e.getWhoClicked();
-        if(book == null || enchantedItem == null || book.getType().isAir() || enchantedItem.getType().isAir()
-                || (book.getType() != Material.ENCHANTED_BOOK && book.getType() != Material.BOOK) ||
-                enchantedItem.getType() == Material.BOOK || enchantedItem.getType() == Material.ENCHANTED_BOOK) return;
-        System.out.println("book: " + book);
-        System.out.println("enchantedItem: " + enchantedItem);
+        if (book == null 
+                || enchantedItem == null
+                || book.getType().isAir()
+                || enchantedItem.getType().isAir()
+                || (book.getType() != Material.ENCHANTED_BOOK && book.getType() != Material.BOOK)
+                || enchantedItem.getType() == Material.BOOK
+                || enchantedItem.getType() == Material.ENCHANTED_BOOK) return;
+        //System.out.println("book: " + book);
+        //System.out.println("enchantedItem: " + enchantedItem);
+        if (!getConfig().getBoolean("disenchant_settings.enabled")) return;
         Disenchanter disenchanter = new Disenchanter(player, book, enchantedItem, new ConfigManager(this));
         if(!disenchanter.isValidItemStacks()) return;
         disenchanter.RemoveEnchantment();
